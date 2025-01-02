52a9e7270b83a837fd41029b55a221d521283c72
Nihal Rawat <nihal.riya03@gmail.com>
2025-01-02 15:08:57 +0530

    Add logo to sidebar and navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,17 @@
 import { Link, useLocation } from "react-router-dom";
+import Logo from "../../public/logo.jpg"
 
 function Sidebar() {
     const location = useLocation();
 
     return (
-        <div className="bg-gray-800 text-white lg:w-96 lg:h-full sm:flex lg:flex-col flex px-16 h-fit items-center justify-between lg:px-0 py-6">
+        <div className="bg-gray-800 text-white lg:w-96 lg:h-full sm:flex lg:flex-col flex sm:px-10 px-6 h-fit items-center justify-between lg:px-0 py-6">
             <Link
                 to="/"
-                className="text-lg lg:text-4xl font-bold lg:px-12 lg:py-6 border-b border-gray-700 lg:border-none"
+                className="text-lg lg:text-4xl font-bold lg:py-6 border-b border-gray-700 lg:border-none flex items-center gap-2"
             >
-                CF User Insights
+                <img className="h-12" src={Logo} alt="" />
+                <span>CF User Insights</span>
             </Link>
             <nav className="lg:flex-grow">
                 <ul className="lg:mx-6 lg:mt-4 lg:space-y-4 flex space-x-4 lg:flex-col sm:flex-row flex-col lg:space-x-0 ">
